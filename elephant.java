3162c1e11c0466b7b917bfd8fd1166689aec8dd8
Nich0las-Ng <341149722@gapps.yrdsb.ca>
2023-11-23 09:15:08 -0500

    move
    made elephant and apple move

--- elephant.java
+++ elephant.java
@@ -15,6 +15,11 @@ public class Elephant extends Actor
     public void act()
     {
         // Add your action code here.
-        move(2);
+        if(Greenfoot.isKeyDown("a")){
+            move(-2);
+        }
+        if(Greenfoot.isKeyDown("d")){
+            move(2);
+        }
     }
 }
